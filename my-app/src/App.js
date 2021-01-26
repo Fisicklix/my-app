a2d643580e88e014feaa1c71c32270d0ba4c9cbb
Gary Simken <gary.simken@usach.cl>
2021-01-26 12:45:42 -0300

    version app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,31 +6,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props);
-    this.state = { username: '' };
-    this.count=0;
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  myChangeHandler = (event) => {
-    this.setState({username: event.target.value});
-    this.count=this.count+1
-  }
+
   render() {
     return (
       <div>
-      <form>
-      <h1>Hello {this.state.username}</h1>
-      <p>Enter your name:</p>
-      <input
-        type='text'
-        onChange={this.myChangeHandler}
-      />
-      
-      </form>
-      <p>Tu nombre tiene {this.count} caracteres</p>
+        <h3>Tareas pendientes</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            ¿Qué se necesita hacer?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Añadir #{this.state.items.length + 1}
+          </button>
+        </form>
       </div>
- 
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     );
   }
 }
