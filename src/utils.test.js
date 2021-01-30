b57f12f9f9d44fa66aa8a7ae6f22a99ef008a0a1
Gary Simken <gary.simken@usach.cl>
2021-01-30 17:24:16 -0300

    feat: New test

--- src/utils.test.js
+++ src/utils.test.js
@@ -2,4 +2,5 @@ import {transformHoursToMinutes} from './utils.js';
 
 test('La transformacion de horas a minutos es correcta', () => {
     expect (transformHoursToMinutes(1)).toBe('60 Minutos')
+    expect (transformHoursToMinutes(2)).toBe('120 Minutos')
 });
